338fed31066f213a459ed5041aa80b07b4019f02
Rangaraj Tirumala <rangaraj.t@gmail.com>
2019-06-03 23:52:05 -0400

    implemented file traversal

--- corpus_handlers/bnc.py
+++ corpus_handlers/bnc.py
@@ -1,3 +1,5 @@
+import os
+
 import nltk
 from nltk import word_tokenize
 from nltk import sent_tokenize
@@ -5,4 +7,17 @@ from nltk import pos_tag
 
 import bs4 as BeautifulSoup
 
-bnc_dir = 'This is a sent'
+bnc_dir = 'corpora/2554/2554/download/Texts/'
+
+def get_file_paths(path:str, extension:str)->list:
+    fnames = []
+    for root, dirs, files in os.walk(path):
+        for f in files:
+            if not f.endswith(extension):
+                continue
+            
+            fnames.append(os.path.join(root, f))
+    return fnames
+
+fnames = get_file_paths(bnc_dir, '.xml')
+print(len(fnames))
